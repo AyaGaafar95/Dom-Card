a936d0dd27c1a0c028d169d14753f653bd0051db
AyaGaafar95 <ayagafar88@gmail.com>
2024-09-25 20:30:22 +0300

    style card and container and images

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let names = ['moody', 'saaed', 'DoDo','lolo','roma','yoya','koki','Jamal'];
+// let colors =['#d8f3dc','#f6bd60','#f5cac3','#ecf39e','#fbf8cc','#cfbaf0','#ffe156','#caf0f8',];
 let colors =['#f6bd60','#caf0f8','#f5cac3','#ecf39e','#fbf8cc','#cfbaf0']
+let avatars = ['images/Avatars-Bakar.png','images/Avatars.png','images/Avatars (5).png','images/Avatars (1).png','images/Avatars (3).png','images/Avatars (1).png','images/Avatars (3).png','images/Avatars.png']
 let container = document.createElement('div');
 document.body.appendChild(container);
 container.style.textAlign = 'center'
 
-function createCards(name , color){
+function createCards(name , color, images){
 // create element
 // بقوله كل حاجه هتكون عبارة عن اايه
     let card = document.createElement('div');
@@ -15,39 +17,49 @@ function createCards(name , color){
 // create content
 let head = document.createTextNode(name);
 let ageContent = document.createTextNode('age');
-// image.src ='https://plus.unsplash.com/premium_photo-1723672941904-dcd84fb8feda?q=80&w=1455&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-image.src = '/Avatars-Bakar.png'
+image.src = images
 title.appendChild(head)
 age.appendChild(ageContent)
 
-// style
+// style container
 container.style.display = 'flex'
-card.style.width = '200px'; // ensure equal width and height for a circle
+container.style.flexWrap = 'Wrap'
+container.style.justifyContent = 'center'
+
+// card style
+
+card.style.width = '200px'; 
 card.style.height = '200px'; 
-// card.style.width = '15rem'
-card.style.padding = '10px'
 card.style.background = color;
 card.style.margin = '10px'
-card.style.flexWrap = 'wrap'
 card.style.borderRadius = '50%'
-title.style.margin = '0px'
-image.style.width = '70%'
+card.style.display = 'flex'
+card.style.flexDirection = 'column'
+card.style.justifyContent = 'center'
+card.style.alignItems = 'center'
 
+title.style.margin = '0px';
+age.style.margin ='0px'
+
+
+// image style
+image.style.width = '100px'
+image.style.height = '100px'
+image.style.borderRadius = '50%'
+
+// Append Elements to card
 
 card.appendChild(title)
 card.appendChild(age)
 card.appendChild(image)
 container.appendChild(card)
 }
-//  let color ;
 
-// for (let index = 0; index < colors.length; index++) {
-//     color = colors[index];
-//     card.style.background = color
-// }
 
-// TODO: If the names array has more items than the colors
+
+// TODO: If the names array has more items than the colors ==> % colors.length
 for(i = 0 ; i< names.length ; i++){
     let color = colors[i % colors.length];
-    createCards(names[i], color);
+    let images = avatars[i]
+    createCards(names[i], color,images);
 }
